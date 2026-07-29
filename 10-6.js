30648cf2f4905de03eb1fa11f1e3d11a86ef62f0
nguyenhoanganh09110505-del <nguyenhoanganh09110505@gmail.com>
2026-07-29 09:17:19 +0900

    Add files via upload

---
+++ 10-6.js
@@ -0,0 +1,7 @@
+let Math = Number(prompt("Nhập điểm môn Toán"));
+let Physics = Number(prompt("Nhập điểm môn Vật Lý"));
+let Chemistry = Number(prompt("Nhập điểm môn Hóa"));
+
+let average = (Math + Physics + Chemistry) / 3;
+
+alert("Điểm trung bình:" + average);
